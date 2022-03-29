b949a26c6d32c267197c24eeb502567ce3427bf1
j.a.u.m.06@hotmail.com <jaum1991>
2022-03-29 16:53:18 -0500

    Se crea nuevo branch y se genera el patron de comportamiento observer

---
+++ app/src/main/java/com/cristianvillamil/platziwallet/ui/observable/Observable.kt
@@ -0,0 +1,8 @@
+package com.cristianvillamil.platziwallet.ui.observable
+
+interface Observable {
+
+    fun addObserver(observer: Observer)
+    fun removeObserver(observer: Observer)
+    fun notifyObservers(newValue : Double)
+}
